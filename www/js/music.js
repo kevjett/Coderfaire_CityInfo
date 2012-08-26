2c32f44255ade61defd115b6864e0f1027d53592
Kevin Jett <kevjett@gmail.com>
2012-08-26 14:31:44 -0500

    add global variables

--- www/js/music.js
+++ www/js/music.js
@@ -4,23 +4,30 @@
 
  function postProcessing(data) {
    var json = $.xml2json(data);
-   //alert(json.event[0].event_date);
+   var event_date = '';
    $.each(json.event, function(index, value) { 
-	   $('#music-list').append('<li>Item ' + index + '</li>');
+   		if (event_date!=value.event_date) {
+   			$('#music-list').append('<li data-role="list-divider">' + value.event_date + '</li>');
+   			event_date = value.event_date;
+   		}
+   		var artists = jQuery.makeArray(value.artists);
+   		var artist = jQuery.makeArray(artists[0].artist);
+   		$('#music-list').append('<li><a href="' + value.event_url + '">' + artist[0].artist_name + ' @ ' + value.venue.venue_name + '</a></li>');
 	});
    $('#music-list').listview('refresh');
    $.mobile.hidePageLoadingMsg();
  }
 
 
-function getValues(){
+function getData(){
 		$.mobile.showPageLoadingMsg();
+
          $.ajax({
             url: 'proxy.php',
             type: 'GET',
             //'http://api.jambase.com/search?zip=37209&apikey=9cr62zp4rfuseug5zbn428k4&radius=15&startDate=08/26/12&endDate=08/29/12'
             data: {
-				proxy_url: 'http://api.jambase.com/search?zip=37209&apikey=9cr62zp4rfuseug5zbn428k4&radius=15&startDate=08/26/12&endDate=08/29/12'
+				proxy_url: 'http://api.jambase.com/search?zip=' + zip + '&apikey=9cr62zp4rfuseug5zbn428k4&radius=15&startDate=' + today + '&endDate=' + later
 			},
             cache: false,
             success: function(data) {postProcessing(data);},
@@ -30,6 +37,5 @@ function getValues(){
 
 
 $( document ).delegate("#music", "pageshow", function() {
- getValues();
-  //alert("test");
+ getData();
 });
