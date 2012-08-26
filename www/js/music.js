b3391de0f975305f72a263e4707b6a2dd1a29f1a
Kevin Jett <kevjett@gmail.com>
2012-08-26 15:33:12 -0500

    update music and running

--- www/js/music.js
+++ www/js/music.js
@@ -2,41 +2,36 @@
 // This file handles javascript calls for the Music page of the application
 // This file has to be included in the head of index.html
 
- function postProcessing(data) {
-   var json = $.xml2json(data);
-   var event_date = '';
-   $('#music-list').empty();
-   $.each(json.event, function(index, value) { 
-   		if (event_date!=value.event_date) {
-   			$('#music-list').append('<li data-role="list-divider">' + value.event_date + '</li>');
-   			event_date = value.event_date;
-   		}
-   		var artists = jQuery.makeArray(value.artists);
-   		var artist = jQuery.makeArray(artists[0].artist);
-   		$('#music-list').append('<li><a href="' + value.event_url + '">' + artist[0].artist_name + ' @ ' + value.venue.venue_name + '</a></li>');
-	});
-   $('#music-list').listview('refresh');
-   $.mobile.hidePageLoadingMsg();
- }
-
-
-function getData(){
-		$.mobile.showPageLoadingMsg();
-
-         $.ajax({
-            url: 'proxy.php',
-            type: 'GET',
-            //'http://api.jambase.com/search?zip=37209&apikey=9cr62zp4rfuseug5zbn428k4&radius=15&startDate=08/26/12&endDate=08/29/12'
-            data: {
-				proxy_url: 'http://api.jambase.com/search?zip=' + zip + '&apikey=9cr62zp4rfuseug5zbn428k4&radius=15&startDate=' + today + '&endDate=' + later
-			},
-            cache: false,
-            success: function(data) {postProcessing(data);},
-            async:true,
-            });
-    };	
-
 
 $( document ).delegate("#music", "pageshow", function() {
- getData();
+   $.mobile.showPageLoadingMsg();
+   $.ajax({
+      url: 'proxy.php',
+      dataType:'xml',
+      type: 'GET',
+      data: {
+         proxy_url: 'http://api.jambase.com/search?zip=' + zip + '&apikey=9cr62zp4rfuseug5zbn428k4&radius=15&startDate=' + today + '&endDate=' + later
+      },
+      cache: false,
+      success: function(data) {
+         var json = $.xml2json(data);
+         var event_date = '';
+         $('#music-list').empty();
+         $.each(json.event, function(index, value) { 
+               if (event_date!=value.event_date) {
+                  $('#music-list').append('<li data-role="list-divider">' + value.event_date + '</li>');
+                  event_date = value.event_date;
+               }
+               var artists = jQuery.makeArray(value.artists);
+               var artist = jQuery.makeArray(artists[0].artist);
+               $('#music-list').append('<li><a href="' + value.event_url + '">' + artist[0].artist_name + ' @ ' + value.venue.venue_name + '</a></li>');
+         });
+         $('#music-list').listview('refresh');
+         $.mobile.hidePageLoadingMsg();
+      },
+      error: function(xhr, textStatus, errorThrown) { 
+         $.mobile.hidePageLoadingMsg(); 
+      },
+      async:true,
+   });
 });
--- www/js/running.js
+++ www/js/running.js
@@ -2,40 +2,37 @@
 // This file handles javascript calls for the Music page of the application
 // This file has to be included in the head of index.html
 
- function postProcessing(data) {
-   var json = $.xml2json(data);
-   var event_date = '';
-   $('#running-events').empty();
-   $.each(json.results.result, function(index, value) { 
-   		if (event_date!=value.meta.startDate) {
-            event_date = value.meta.startDate;
-   			$('#running-events').append('<li data-role="list-divider">' + event_date.substr(5,2) + '/' + event_date.substr(8,2) + '/' + event_date.substr(0,4) + '</li>');
-   		}
-   		$('#running-events').append('<li><a href="' + value.url + '">' + value.meta.assetName + '</a></li>');
-	});
-   $('#running-events').listview('refresh');
-   $.mobile.hidePageLoadingMsg();
- }
-
-
-function getData(){
-		$.mobile.showPageLoadingMsg();
-      var r_today = "" + ("0" + (t.getMonth()+1)).slice(-2) + "/" + ("0" + t.getDate()).slice(-2) + "/" + t.getFullYear();
-      var r_later = "" + ("0" + (t.getMonth()+2)).slice(-2) + "/" + ("0" + t.getDate()).slice(-2) + "/" + t.getFullYear();
-         $.ajax({
-            url: 'proxy.php',
-            type: 'GET',
-            data: {
-				proxy_url: 'http://api.amp.active.com/search?k=&f=activities&v=xml&l=' + zip + '&r=15&s=date_asc&m=meta:startDate:daterange:08/26/2012..09/31/2012+meta:channel%3DRunning+meta:splitMediaType%3DEvent&api_key=wuhmn9ye94xn3xnteudxsavw'
-			},
-            cache: false,
-            success: function(data) {postProcessing(data);},
-            failure: function() { $.mobile.hidePageLoadingMsg(); },
-            async:true,
-            });
-    };	
-
 
 $( document ).delegate("#runningEvents", "pageshow", function() {
- getData();
+   $.mobile.showPageLoadingMsg();
+   var r_today = "" + ("0" + (t.getMonth()+1)).slice(-2) + "/" + ("0" + t.getDate()).slice(-2) + "/" + t.getFullYear();
+   var r_later = "" + ("0" + (t.getMonth()+2)).slice(-2) + "/" + ("0" + t.getDate()).slice(-2) + "/" + t.getFullYear();
+   $.ajax({
+      url: 'proxy.php',
+      type: 'GET',
+      data: {
+         proxy_url: 'http://api.amp.active.com/search?k=&f=activities&v=xml&l=' + zip + '&r=15&s=date_asc&m=meta:startDate:daterange:' + r_today + '..' + r_later + '+meta:channel%3DRunning+meta:splitMediaType%3DEvent&api_key=7mrkcvgjeaj69n7hyaqd93ne'
+      },
+      cache: false,
+      success: function(data) {
+         $('#running-events').empty();
+         var json = $.xml2json(data);
+         if (json.totalResults=='0') {
+            $('#running-events').append('<li>No events found</li>');
+         } else {
+            var event_date = '';
+            $.each(json.results.result, function(index, value) { 
+                  if (event_date!=value.meta.startDate) {
+                     event_date = value.meta.startDate;
+                     $('#running-events').append('<li data-role="list-divider">' + event_date.substr(5,2) + '/' + event_date.substr(8,2) + '/' + event_date.substr(0,4) + '</li>');
+                  }
+                  $('#running-events').append('<li><a href="' + value.url + '">' + value.meta.assetName + '</a></li>');
+            });
+         }
+         $('#running-events').listview('refresh');
+         $.mobile.hidePageLoadingMsg();
+      },
+      failure: function() { $.mobile.hidePageLoadingMsg(); },
+      async:true,
+   });
 });
