b52c3999c9cba2acdf54ded60b555c510fd8ff8e
Sean Mumford <thepsion5@conakry.dreamhost.com>
2012-08-26 11:48:57 -0700

    adding movie listing

--- www/js/movieTimes.js
+++ www/js/movieTimes.js
@@ -3,8 +3,21 @@
 // This file has to be included in the head of index.html
 
 $( document ).delegate("#movieTimes", "pageshow", function() {
+    var zip = '37027';
+    var $movieList = $('#movies-list');
+    var data = {
+        proxy_url: 'http://www.fandango.com/rss/moviesnearme_' + zip + '.rss'
+    };
+    $.get('proxy.php', data, function(response){
+        var json = $.xml2json(response);
+        console.log(json);
+        displayMoviesNearMe(json.channel.item);
+    })
   
-  // Do stuff for this page here.
-  
-  
+    function displayMoviesNearMe(json) {
+        $.each(json, function(index, item) { 
+            $movieList.append('<li>' + item.title + '</li>');
+        });
+        $movieList.listview('refresh');
+    } 
 });
