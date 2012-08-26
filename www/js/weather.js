7dd2c2599085146617f1395a1828e7697f3efce0
dskaggs <dan.skaggs@web-meister.com>
2012-08-26 12:02:08 -0500

    Added stub of js file for the weather page

---
+++ www/js/weather.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+// This file handles javascript calls for the Weather page of the application
+// This file has to be included in the head of index.html
+
+$( document ).delegate("#weather", "pageshow", function() {
+  
+  // Do stuff for this page here.
+  
+  
+});
